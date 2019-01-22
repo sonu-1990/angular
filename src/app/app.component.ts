45db473a6c05679b195875dea7db7d41ea4d548d
spokade@gmail.com <spokade@gmail.com>
2019-01-22 18:52:15 +0530

    Study ngif and nffor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  isLoggedIn:boolean = false;
+  users:string[] = ["Sonu", "Sneha", "Neha"];
+  students:any[] = [
+    {id:1, name:"Sonu", salary:"10k"},
+    {id:2, name:"Sneha", salary:"12k"},
+    {id:3, name:"Neha", salary:"30k"},
+  ]
 }
